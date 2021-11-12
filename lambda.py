ec0367a3d5e83d9edfcd48f0ee5ce091a14e3a5e
18635 <emmaw4725@outlook.com>
2021-11-11 21:27:17 -0500

    first file from PyCharm

---
+++ lambda.py
@@ -0,0 +1,14 @@
+"""lambda表达式-创建匿名函数；"""
+"""
+用处：需要一个函数，但是又不想费神去命名这个函数；
+通常在这个函数只使用一次的场景下，可以指定短小的回调函数。
+"""
+"""
+语法：
+result = lambda [arg1 [, arg2, .... , argm]]: expression
+"""
+import math
+result = lambda r:math.pi*r*r
+
+print(result(10))
+
