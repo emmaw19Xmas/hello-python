98aa5ab0f2da6717b5d5a21c89ca1f1a21192a32
18635 <emmaw4725@outlook.com>
2021-11-20 18:12:23 -0500

    modules and packages

--- decorator_demo.py
+++ decorator_demo.py
@@ -2,16 +2,32 @@
 # @Author  : 18635
 # @Time    : 11/20/2021 5:19 PM
 # @Function:
+'''
+fesf
+'''
+
+
+import datetime
+
 def timer(func):
-    def inner():
-        print("timer start")
-        func()
-        print("timer end")
+    # 如果被装饰函数有参数，那么需要内函数加形参。
+    # 但往往无法确定被装饰的参数的参数数量，所以把两个地方的参数全部换成不定长的参数
+    def inner(*args, **kwargs):
+        starttime = datetime.datetime.now()
+        func(*args, **kwargs)
+        endtime = datetime.datetime.now()
+        print(f"execute time{endtime-starttime}")
     return inner
 
 @timer # 添加装饰器之后，每当该函数被调用，timer就会启动
 def hogwarts():
     print("hogwarts")
 
+@timer
+def add(a,b):
+    print(a+b)
+
 
 hogwarts()
+
+add(10348235,234082)
