593f4b8e827782188944a138906f55ed9db70501
18635 <emmaw4725@outlook.com>
2021-11-23 22:19:43 -0500

    modules and packages

--- Unittest_demo/test_search.py
+++ Unittest_demo/test_search.py
@@ -2,7 +2,30 @@
 # @Author  : 18635
 # @Time    : 11/23/2021 9:17 PM
 # @Function:
+import unittest
 
-def search():
-    print("search method")
 
+class Search():
+    def search(self):
+        print("search method")
+        return 0
+
+class TestSearch(unittest.TestCase):
+
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.s= Search()
+
+    def setUp(self) -> None:
+        self.s= Search()
+
+    def test_if0(self):
+        #s = Search()
+        self.assertEqual(self.s.search(), 0,"verify if the result is 0")
+
+    def test_iflargert2(self):
+        #s = Search()
+        self.assertGreater(s.search, -2, "verfiy if the result is larger than -2")
+
+        #https://cloud.tencent.com/developer/article/1630094
